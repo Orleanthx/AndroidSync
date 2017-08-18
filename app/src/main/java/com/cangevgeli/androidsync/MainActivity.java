0c9a25e6f8d4464e4e5880a46ab6cbcb6b9bb2b5
CanGevgeli <cangevgeli@gmail.com>
2017-08-18 03:02:25 +0200

    Ringtone Functionalities OK

--- app/src/main/java/com/cangevgeli/androidsync/MainActivity.java
+++ app/src/main/java/com/cangevgeli/androidsync/MainActivity.java
@@ -1,21 +1,32 @@
 package com.cangevgeli.androidsync;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +55,91 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        /**Contact Ringtones**/
+        String[] myProjection = new String[]{ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                                             ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                                             ContactsContract.CommonDataKinds.Phone.CUSTOM_RINGTONE};
+
+        Cursor myCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, myProjection, null, null, null);
+        while (myCursor.moveToNext())
+        {
+            if(myCursor.getString(2) != null ) {
+               System.out.println(myCursor.getString(0)
+                        + " " + myCursor.getString(1)
+                        + " " + myCursor.getString(2));
+            }
+        }
+        myCursor.close();
+        /**Contact Ringtones**/
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        /**Ringtones**/
+        RingtoneManager myRingtones = new RingtoneManager(this);
+        myRingtones.setType(RingtoneManager.TYPE_RINGTONE);
+        Cursor myRingtonesCursor = myRingtones.getCursor();
+        while (myRingtonesCursor.moveToNext())
+        {
+            if(!myRingtonesCursor.getString(2).contains("content://media/internal/")) {
+                System.out.println(myRingtonesCursor.getString(0)
+                           + " " + myRingtonesCursor.getString(1)
+                           + " " + myRingtonesCursor.getString(2));
+            }
+        }
+        myRingtonesCursor.close();
+        /**Ringtones**/
     }
 
     @Override
@@ -68,4 +163,46 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
